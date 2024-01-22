badb67997c7c493c0a36f5d59fc62270402ae6bd
CaioFranco <caiovinicius09111996@gmail.com>
2024-01-22 18:39:57 -0300

    att aula 11

---
+++ Aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu veiculo é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou avelocidade permitida, MULTADO!!!!`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
---
+++ Aula11/ex09.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você éBrasileiro ! ')
+}
+else{
+    console.log('Você é Estrangeiro !')
+}
