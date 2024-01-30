64f16cea43209f253bf3d56a4d3cb4c686c10bd6
CaioFranco <caiovinicius09111996@gmail.com>
2024-01-29 23:14:03 -0300

    exercicio de js

--- Aula12ex/js/arquivo.js
+++ Aula12ex/js/arquivo.js
@@ -1,6 +1,24 @@
-var agr = new Date()
-var hr = agr.getHours()
+function carregar() {
+    
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
 
-if (hr >= 12) {
-    document.body.style.backgroundColor = '#E6A720'
+    var data = new Date()
+    var hr = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hr} horas.`
+    if (hr >= 12 && hr < 18) {
+        img.src = 'modelo/tarde.png'
+        document.body.style.background = '#83674f'
+    } else if (hr >= 18 && hr < 24) {
+        img.src = 'modelo/noite.png'
+        document.body.style.background = '#1a2239'
+    } else {
+        img.src = 'modelo/manha.png'
+        document.body.style.background = '#e0d6b3'
+    }
 }
+
+
+
+
